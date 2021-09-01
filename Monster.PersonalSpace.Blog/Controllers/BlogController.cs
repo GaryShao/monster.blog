237370d49b80cea433a3474973d46a51d6cd7db2
gary <gary_shau@163.com>
2021-09-01 12:53:01 +0800

    demo

---
+++ Monster.PersonalSpace.Blog/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Monster.PersonalSpace.Blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class BlogController : ControllerBase
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "content")]
+        public IEnumerable<object> GetBlogContent([FromQuery] string blogId)
+        {
+            const string logInfo = "visiting blog file, file id is {BlogId}";
+            _logger.LogInformation(logInfo, blogId);
+            return null;
+        }
+    }
+}
